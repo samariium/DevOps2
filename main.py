ba2ed19b1de9b8cec2ac2ce824ac5d509f0b199c
samarr <143421234+samariium@users.noreply.github.com>
2023-09-25 22:14:53 +0530

    for creating a excel sheet

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import openpyxl
+
+# Create a new Excel workbook
+workbook = openpyxl.Workbook()
+
+# Create a new sheet
+sheet = workbook.active
+
+# Add headers
+sheet["A1"] = "Date"
+sheet["B1"] = "Student Name"
+sheet["C1"] = "Attendance Status"
+
+# Save the workbook
+workbook.save("attendance.xlsx")
 
