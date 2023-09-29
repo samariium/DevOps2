ac901e461a512d4365cb836bcef9689b33d05ff0
samarr <143421234+samariium@users.noreply.github.com>
2023-09-29 20:32:54 +0530

    delete previous code

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-# to save a excel file/sheet for this 
-import openpyxl
-
-# Create a new Excel workbook
-workbook = openpyxl.Workbook()
-
-# Create a new sheet
-sheet = workbook.active
-
-# Add headers
-sheet["A1"] = "Date"
-sheet["B1"] = "Student Name"
-sheet["C1"] = "Attendance Status"
-
-# Save the workbook
-workbook.save("attendance.xlsx")
 
