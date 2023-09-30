929bc26fcacb2064f2e0c1dac7328e3b1317b648
samarr <143421234+samariium@users.noreply.github.com>
2023-09-30 22:46:17 +0530

    Update main.py  till line no - 30

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ def input_attendance():
         return None, None
     attendance_status = simpledialog.askstring("Attendance Tracker", "Enter attendance status (Present/Absent):")
     return student_name, attendance_status
+# Function to load and display previous attendance records
+def load_previous_attendance():
+    try:
+        workbook = load_workbook("attendance.xlsx")
+        sheet = workbook.active
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            date, student_name, attendance_status = row
+            attendance_listbox.insert(tk.END, f"Date: {date}, Student: {student_name}, Status: {attendance_status}")
+        workbook.close()
+    except FileNotFoundError:
+        messagebox.showinfo("Info", "No previous attendance records found.")
+
+# Generate a unique filename based on the current date and time
+today = date.today()
+file_suffix = today.strftime("%Y%m%d")
+file_name = f"attendance_{file_suffix}.xlsx"
 
